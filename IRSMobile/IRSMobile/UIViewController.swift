cd5a9f8e36273299035790ca9f84ca8d98e59393
hungdn <doanngochung55pmxd@gmail.com>
2018-01-05 23:59:35 +0700

    run complete on xcode 8.3.3

--- IRSMobile/IRSMobile/UIViewController.swift
+++ IRSMobile/IRSMobile/UIViewController.swift
@@ -20,17 +20,15 @@ extension UIViewController {
     func removeNavigationBarItem() {
         self.navigationItem.leftBarButtonItem = nil
         self.slideMenuController()?.removeLeftGestures()
-   
+        
     }
     func setNavigationBar(title : String) {
         
         let image = UIImage(named: "ic_menu_black_24dp")
         let screenSize: CGRect = UIScreen.main.bounds
-        //let navBar = NavigationBar(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: 60))
-         let navBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: screenSize.width, height: 60))
-       
+        let navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: 60))
+        
         let navItem = UINavigationItem(title: title)
-        navBar.setTitleVerticalPositionAdjustment(CGFloat(7), for: UIBarMetrics.default)
         let doneItem = UIBarButtonItem(image: image, style: UIBarButtonItemStyle.plain, target: nil, action: #selector(done))
         navItem.leftBarButtonItem = doneItem
         navBar.setItems([navItem], animated: false)
@@ -40,6 +38,6 @@ extension UIViewController {
     func done() {
         self.slideMenuController()?.openLeft()
     }
-
-
+    
+    
 }
